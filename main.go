a1203423a118c73dc2053e97bee2a3a53a922a16
user <user@mail.com>
2022-03-20 21:17:16 +0800

    add README

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type animal interface {
+	mover
+	eater
+}
+type mover interface {
+	move()
+}
+
+type eater interface {
+	eat(string)
+}
+type cat struct {
+	name string
+	feet int
+}
+
+func (c *cat) mover() {
+	fmt.Printf("走猫步。。。")
+}
+func (c *cat) eater(food string) {
+	fmt.Printf("吃了%s, ", food)
+}
+
+func main() {
+	var a1 animal
+	d1 := cat{
+		"bosimao",
+		1,
+	}
+	d2 := &cat{
+		name: "jilaolian",
+		feet: 2,
+	}
+	a1 = d1
+	fmt.Println(d1)
+	fmt.Println(d2)
+
+	d1.mover()
+	d1.eater("猫屎")
+}
