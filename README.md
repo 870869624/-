764f0cb3ecb2957eb0ccc3fa8729552242d66f62
景海军 <jing_haijuntw@topsec.com.cn>
2025-07-04 17:30:45 +0800

    feat: 添加完整day5 day6学习内容

---
+++ README.md
@@ -0,0 +1,3 @@
+# [菜鸟教程](https://www.runoob.com/html/html-links.html)
+
+# [学习web开发](https://developer.mozilla.org/zh-CN/docs/Learn_web_development/Core/Structuring_content/HTML_images)
--- demo/day1/scripts/main.js
+++ demo/day1/scripts/main.js
@@ -2,14 +2,19 @@
 
 const myImg = document.querySelector('img');
 
-myImg.onclick = () =>{
-    const mySrc = myImg.getAttribute('src');
-    if(mySrc === './images/image.png'){
-        myImg.setAttribute('src', './image/image.png');
-    }else{
-        myImg.setAttribute('src', './image/image1.png');
+
+myImg.onclick = () => {
+    const currentSrc = myImg.getAttribute('src');
+    const image1 = './image/image.png';
+    const image2 = './image/image1.png';
+
+    // 切换图片
+    if (currentSrc === image1) {
+        myImg.setAttribute('src', image2);
+    } else {
+        myImg.setAttribute('src', image1);
     }
-}
+};
 
 var button = document.querySelector('button');
 var heading = document.querySelector('h1');
